c27d93b2096fa4e51a0580aa3723a052d0006a39
Kobe Davis <kodavis@pdx.edu>
2019-12-20 12:50:29 -0800

    Program now applies a guassian blur before the sobel op, and also removes alpha=0 pixels
    Since the sobel operator is somewhat sensitive to noise, I added in a gaussian convolution to remove higher frequencies. This drastically improves the clarity of the final image, although this is still dependent on the configuration of the gaussian kernel.
    
    Additionally, I've added to the grayscale portion so that the pixel RGB values are now turned off if the alpha value is 0. This removes any invisible edges hidden within the image.

--- main.lua
+++ main.lua
@@ -27,23 +27,47 @@ end
 function convolve(image, kernel, rows, cols)
 	local gradImg = {}
 	local crop = #kernel - 1
+	local kRadius = math.floor(kRadius/2.0)
+	
 	for i = 1, rows-crop do
 		gradImg[i] = {}
 		for j = 1, cols-crop do
+		
 			local cutout = {}
-			cutout[1] = {image[i][j].lum, image[i][j+1].lum, image[i][j+2].lum}
-			cutout[2] = {image[i+1][j].lum, image[i+1][j+1].lum, image[i+1][j+2].lum}
-			cutout[3] = {image[i+2][j].lum, image[i+2][j+1].lum, image[i+2][j+2].lum}
+			for r = 0, crop do
+				cutout[r+1] = {}
+				for c = 0, crop do
+					cutout[r+1][c+1] = image[i+r][j+c].lum
+				end
+			end
 			
-			r, g, b, a, _ = unpack(image[i+1][j+1])
+			r, g, b, a, _ = unpack(image[i+kRadius][j+kRadius])
 			gradImg[i][j] = {red=r, green=g, blue=b, alpha=a, lum=weightedSum(cutout, kernel)}
 		end
 	end
 	return gradImg
 end
 
+function generateGaussian(kernel, dim, radius)
+	stdev = 1.0
+	pi = 355.0/113.0
+	constant = 1.0/(2.0*pi*math.pow(stdev, 2))
+	
+	for i = -radius, radius do
+		kernel[i+radius+1] = {}
+		for j = -radius, radius do
+			kernel[i+radius+1][j+radius+1] = constant*(1.0/math.exp( (math.pow(i,2)+math.pow(j,2)) / (2*math.pow(stdev,2)) ))
+		end
+	end
+end
+
 function love.load()
 	love.window.setMode(400, 600)
+	
+	kGaussian = {}
+	kDim = 3
+	kRadius = math.floor(kDim/2.0)
+	generateGaussian(kGaussian, kDim, kRadius)
 
 	kX = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}
 	kY = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}}
@@ -57,13 +81,22 @@ function love.load()
 		arrImg[i] = {}
 		for j = 1, cols do
 			r, g, b, a = origImg:getPixel(j-1, i-1) -- LOVE2D uses (x,y) coords, not row-major order
+			if a  == 0 then
+				r = 0
+				g = 0
+				b = 0
+			end
 			local lum = 0.2126*r + 0.7152*g + 0.0722*b
 			arrImg[i][j] = {red=r, green=g, blue=b, alpha=a, lum=lum}
 		end
 	end
 	
-	gradX = convolve(arrImg, kX, rows, cols)
-	gradY = convolve(arrImg, kY, rows, cols)
+	blurImg = convolve(arrImg, kGaussian, rows, cols)
+	rows = #blurImg - 1
+	cols = #blurImg[1] - 1
+	
+	gradX = convolve(blurImg, kX, rows, cols)
+	gradY = convolve(blurImg, kY, rows, cols)
 	rows = #gradX - 1
 	cols = #gradX[1] - 1
 	
